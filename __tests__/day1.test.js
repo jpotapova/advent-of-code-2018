bf979c751e1323ae8f101a1c433d5ea7db3c9fc2
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-12-01 22:30:18 +0200

    Change naming to day1

---
+++ __tests__/day1.test.js
@@ -0,0 +1,6 @@
+const day1 = require('../day1');
+describe('Day 1 solution', () => {
+  it('returns 0', () => {
+    expect(day1()).toBe(0);
+  });
+});
---
+++ day1.js
@@ -0,0 +1,5 @@
+function day1(arg) {
+  return 0;
+}
+
+module.exports = day1;

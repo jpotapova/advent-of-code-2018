36150ddfd6efe2139299e852433b2cea5a2992b0
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-12-01 23:53:30 +0200

    Add test case for the real day 1 part 2 data

--- __tests__/day1.test.js
+++ __tests__/day1.test.js
@@ -1,7 +1,7 @@
 const day1 = require('../day1');
 
 describe('Day 1', () => {
-  describe('Part 1', () => {
+  describe('Part 1 example cases', () => {
     it('Frequency changes +1, +1, +1 results in 3', () => {
       expect(day1.calculateFrequency([1, 1, 1])).toBe(3);
     });
@@ -11,15 +11,9 @@ describe('Day 1', () => {
     it('Frequency changes -1, -2, -3 results in -6', () => {
       expect(day1.calculateFrequency([-1, -2, -3])).toBe(-6);
     });
-    it('Frequency changes listed in the file result in 454', () => {
-      const fs = require('fs');
-      const contents = fs.readFileSync('__tests__/day1-input.txt', 'utf8');
-      const changes = contents.split(/\r?\n/).map((change) => parseInt(change, 10));
-      expect(day1.calculateFrequency(changes)).toBe(454);
-    });
   });
 
-  describe('Part 2', () => {
+  describe('Part 2 example cases', () => {
     it('For frequency changes +1 -1 first frequency the device reaches twice is 0', () => {
       expect(day1.frequencyRepeated([1, -1])).toBe(0);
     });
@@ -29,5 +23,23 @@ describe('Day 1', () => {
     it('For frequency changes -6 +3 +8 +5 -6 first frequency the device reaches twice is 10', () => {
       expect(day1.frequencyRepeated([-6, 3, 8, 5, -6])).toBe(5);
     });
+  });
+
+  describe('Part 1 and Part 2 real data', () => {
+    let changes;
+    beforeAll(() => {
+      const fs = require('fs');
+      const contents = fs.readFileSync('__tests__/day1-input.txt', 'utf8');
+      changes = contents.split(/\r?\n/).map((change) => parseInt(change, 10));
+    });
+
+    it('Frequency changes listed in the file result in 454', () => {
+      expect(day1.calculateFrequency(changes)).toBe(454);
+    });
+
+    it('For frequency changes listed in the file frequency the device reaches twice is 10', () => {
+      expect(day1.frequencyRepeated(changes)).toBe(566);
+    });
+
   });
 });

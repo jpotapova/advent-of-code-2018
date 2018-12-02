dd317a63731e0aebd5132f4847b2ef5bbbe15ff1
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-12-02 15:14:05 +0200

    Count symbol occurences in an id

--- day2.js
+++ day2.js
@@ -1,7 +1,29 @@
 const day2 = {
-    containsNtimes(times, id) {
-      return false;
+  symbolOccurences(id) {
+    const length = id.length;
+    const foundValues = {};
+    for (let i = 0; i < length; i++) {
+      if (id[i] in foundValues) {
+        foundValues[id[i]]++
+      } else {
+        foundValues[id[i]] = 1;
+      }
     }
+    return foundValues;
+  },
+  // containsNtimes(times, id) {
+  //   const symbols = Object.keys(foundValues);
+  //   const symbolsLength = symbols.length;
+  //   let foundNtimes = false;
+  //   let j = 0;
+  //   while (!foundNtimes && j < symbolsLength) {
+  //     if (foundValues[symbolsLength[j]] === times) {
+  //       foundNtimes = true;
+  //     }
+  //     j++;
+  //   }
+  //   return foundNtimes;
+  // }
 };
 
 module.exports = day2;

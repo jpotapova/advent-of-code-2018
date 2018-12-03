b57f09227f5350dbdcedb9ba2fb6d185cbe0dda0
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-12-03 22:23:06 +0200

    Day 3, part 1 - implement claim converter to relevant numbers

--- day3.js
+++ day3.js
@@ -1,6 +1,15 @@
 const day3 = {
   processClaim(claim) {
-    return {};
+    let left, top, width, height;
+    const parts = claim.split(' @ ')[1].split(': ');
+    [left, top] = parts[0].split(',');
+    [width, height] = parts[1].split('x');
+    return {
+      left: parseInt(left, 10),
+      top: parseInt(top, 10),
+      width: parseInt(width, 10),
+      height: parseInt(height, 10)
+    };
   }
 };
 

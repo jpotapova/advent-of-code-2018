ba3827c50ccde6603c51856304ffdd1d56c2b992
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-12-04 23:16:40 +0200

    Day 4 - part 1 - list all asleep minutes for each guard

--- __tests__/day4.test.js
+++ __tests__/day4.test.js
@@ -20,41 +20,55 @@ describe('Day 4', () => {
     '[1518-11-05 00:45] falls asleep',
     '[1518-11-05 00:55] wakes up',
   ];
-  const recordsFew = [
-    '[1518-11-01 00:00] Guard #10 begins shift',
-    '[1518-11-01 00:05] falls asleep',
-    '[1518-11-01 00:25] wakes up',
-    '[1518-11-01 23:58] Guard #99 begins shift',
-  ];
-  const dataFew = [
-    {
-      date: '1518-11-01',
-      minute: 0,
-      event: 'shift',
-      guard: 10
-    },
-    {
-      date: '1518-11-01',
-      minute: 5,
-      event: 'asleep',
-      guard: 0
-    },
-    {
-      date: '1518-11-01',
-      minute: 25,
-      event: 'awake',
-      guard: 0
-    },
-    {
-      date: '1518-11-01',
-      minute: -1,
-      event: 'shift',
-      guard: 99
-    }
-  ];
   describe('Part 1 example case', () => {
-    it('Example', () => {
+    it('Process record strings', () => {
+      let recordsFew = [
+        '[1518-11-01 00:00] Guard #10 begins shift',
+        '[1518-11-01 00:05] falls asleep',
+        '[1518-11-01 00:25] wakes up',
+        '[1518-11-01 23:58] Guard #99 begins shift',
+      ];
+      const dataFew = [
+        {
+          date: '1518-11-01',
+          minute: 0,
+          event: 'shift',
+          guard: 10
+        },
+        {
+          date: '1518-11-01',
+          minute: 5,
+          event: 'asleep',
+          guard: 0
+        },
+        {
+          date: '1518-11-01',
+          minute: 25,
+          event: 'awake',
+          guard: 0
+        },
+        {
+          date: '1518-11-01',
+          minute: -1,
+          event: 'shift',
+          guard: 99
+        }
+      ];
       expect(day4.processRecords(recordsFew)).toEqual(dataFew);
+    });
+    it('Count asleep minutes for each guard', () => {
+      let recordsFew = [
+        '[1518-11-01 00:00] Guard #10 begins shift',
+        '[1518-11-01 00:05] falls asleep',
+        '[1518-11-01 00:25] wakes up',
+        '[1518-11-01 23:58] Guard #99 begins shift',
+        '[1518-11-02 00:40] falls asleep',
+        '[1518-11-02 00:50] wakes up',
+      ];
+      expect(day4.getAsleepMinutes(recordsFew)).toEqual({
+        10: [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
+        99: [40, 41, 42, 43, 44, 45, 46, 47, 48, 49]
+      });
     });
   });
 });
--- day4.js
+++ day4.js
@@ -34,6 +34,39 @@ const day4 = {
       };
 
     });
+  },
+  addGuard(mins, guard) {
+    if (guard in mins) return mins;
+    mins[guard] = [];
+    return mins;
+  },
+  addMinutes(mins, guard, start, end) {
+    for (let i = start; i < end; i++) {
+      mins[guard].push(i);
+    }
+    return mins;
+  },
+  getAsleepMinutes(records) {
+    const data = this.processRecords(records);
+    let mins = {};
+    const length = records.length;
+    let guard = 0;
+    let asleep = -1;
+    for (let i = 0; i < length; i++) {
+      let r = data[i];
+      if (r.event === 'shift') {
+        guard = r.guard;
+        mins = this.addGuard(mins, r.guard);
+        asleep = -1;
+      } else if (r.event === 'asleep') {
+        asleep = r.minute;
+      } else {
+        // awake
+        mins = this.addMinutes(mins, guard, asleep, r.minute);
+        asleep = -1;
+      }
+    }
+    return mins;
   }
 };
 
